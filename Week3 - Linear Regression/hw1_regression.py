29b44288950f910a4917944646415cf31a0341eb
asheeshcric <ashiz2013@gmail.com>
2018-08-05 11:15:03 +0545

    [ADD] A reference code for the project and build a boiler template

--- Week3 - Linear Regression/hw1_regression.py
+++ Week3 - Linear Regression/hw1_regression.py
@@ -1,27 +1,35 @@
 import numpy as np
 import sys
 
-lambda_input = int(sys.argv[1])
-sigma2_input = float(sys.argv[2])
-X_train = np.genfromtxt(sys.argv[3], delimiter = ",")
-y_train = np.genfromtxt(sys.argv[4])
-X_test = np.genfromtxt(sys.argv[5], delimiter = ",")
 
 ## Solution for Part 1
-def part1():
+def part1(lambda_input, sigma2_input, X_train, y_train):
     ## Input : Arguments to the function
     ## Return : wRR, Final list of values to write in the file
-    pass
+    return 0
 
-wRR = part1()  # Assuming wRR is returned from the function
-np.savetxt("wRR_" + str(lambda_input) + ".csv", wRR, delimiter="\n") # write output to file
 
 
 ## Solution for Part 2
-def part2():
+def part2(lambda_input, sigma2_input, X_train, y_train, X_test):
     ## Input : Arguments to the function
     ## Return : active, Final list of values to write in the file
-    pass
+    return 0
 
-active = part2()  # Assuming active is returned from the function
-np.savetxt("active_" + str(lambda_input) + "_" + str(int(sigma2_input)) + ".csv", active, delimiter=",") # write output to file
+
+
+if __name__ == '__main__':
+    lambda_input = int(sys.argv[1])
+    sigma2_input = float(sys.argv[2])
+    X_train = np.genfromtxt(sys.argv[3], delimiter=",")
+    y_train = np.genfromtxt(sys.argv[4])
+    X_test = np.genfromtxt(sys.argv[5], delimiter=",")
+
+    # Compute wRR from the first part
+    wRR = part1(lambda_input, sigma2_input, X_train, y_train)
+    np.savetxt("wRR_" + str(lambda_input) + ".csv", wRR, delimiter="\n")  # write output to file
+
+    # Compute sequence for Active Learning from the second part
+    active = part2(lambda_input, sigma2_input, X_train, y_train, X_test.copy())  # Assuming active is returned from the function
+    np.savetxt("active_" + str(lambda_input) + "_" + str(int(sigma2_input)) + ".csv", active,
+               delimiter=",")  # write output to file
