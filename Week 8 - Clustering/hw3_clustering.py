3e76faffaaa4874485c27ed43ba49436d7b8a3ca
asheeshcric <ashiz2013@gmail.com>
2018-08-21 21:04:21 +0545

    [COMPLETE] Project 3 for Gaussian Mixture Model

--- Week 8 - Clustering/hw3_clustering.py
+++ Week 8 - Clustering/hw3_clustering.py
@@ -7,58 +7,54 @@ from scipy.stats import multivariate_normal
 def em_gmm(data, k_clusters):
     max_iterations = 10
 
-    n_rows = data.shape[0]
-    n_cols = data.shape[1]
+    rows = data.shape[0]
+    columns = data.shape[1]
 
-    # initializing the centroids/clusters
-    mu = data[np.random.choice(n_rows, k_clusters, replace=False), :]
+    # Initialize random centroids for clusters
+    myu = data[np.random.choice(rows, k_clusters, replace=False), :]
+    calc_sigma = [np.eye(columns) for i in range(k_clusters)]
 
-    # covariance for each cluster
-    sigma = [np.eye(n_cols) for i in range(k_clusters)]
-
-    # pi - apriori uniform distribution
+    # Initializing Apriori distribution for pi
     pi = np.ones(k_clusters) / k_clusters
 
-    # Initialize phi
-    phi = np.zeros((n_rows, k_clusters))
+    phi = np.zeros((rows, k_clusters))
     for iteration in range(max_iterations):
-        for i in range(n_rows):
-            # This is the normalizing constant (denominator) used while calculating phi(k)
-            norm_constant = np.sum(
-                [pi[k] * multivariate_normal.pdf(data[i], mean=mu[k], cov=sigma[k], allow_singular=True) for k in
+        for i in range(rows):
+            normalization_constant = np.sum(
+                [pi[k] * multivariate_normal.pdf(data[i], mean=myu[k], cov=calc_sigma[k], allow_singular=True) for k in
                  range(k_clusters)])
-            if norm_constant == 0:
+            if normalization_constant == 0:
                 phi[i] = pi / k_clusters
             else:
-                phi[i] = [(pi[k] * multivariate_normal.pdf(data[i], mean=mu[k], cov=sigma[k],
-                                                           allow_singular=True)) / norm_constant for k in
+                phi[i] = [(pi[k] * multivariate_normal.pdf(data[i], mean=myu[k], cov=calc_sigma[k],
+                                                           allow_singular=True)) / normalization_constant for k in
                           range(k_clusters)]
 
         for k in range(k_clusters):
-            nk = np.sum(phi[:, k])
-            pi[k] = nk / n_rows
-            if nk == 0:
-                mu[k] = data[np.random.choice(n_rows, 1, replace=False), :]
-                sigma[k] = np.eye(n_cols)
+            n_k = np.sum(phi[:, k])
+            pi[k] = n_k / rows
+            if n_k == 0:
+                myu[k] = data[np.random.choice(rows, 1, replace=False), :]
+                calc_sigma[k] = np.eye(columns)
             else:
-                mu[k] = np.sum(data * phi[:, k].reshape(n_rows, 1), axis=0) / nk
-                cov_sum = np.zeros((n_cols, n_cols))
+                myu[k] = np.sum(data * phi[:, k].reshape(rows, 1), axis=0) / n_k
+                covariance_sum = np.zeros((columns, columns))
 
-                for i in range(n_rows):
-                    centered_data = data[i] - mu[k]
-                    cov_sum += phi[i, k] * np.outer(centered_data, centered_data)
+                for i in range(rows):
+                    centered_data = data[i] - myu[k]
+                    covariance_sum += phi[i, k] * np.outer(centered_data, centered_data)
 
-                sigma[k] = cov_sum / nk
+                calc_sigma[k] = covariance_sum / n_k
 
         filename = "pi-" + str(iteration + 1) + ".csv"
         np.savetxt(filename, pi, delimiter=",")
         filename = "mu-" + str(iteration + 1) + ".csv"
-        np.savetxt(filename, mu, delimiter=",")  # this must be done at every iteration
+        np.savetxt(filename, myu, delimiter=",")  # this must be done at every iteration
 
         for j in range(k_clusters):  # k is the number of clusters
             filename = "Sigma-" + str(j + 1) + "-" + str(
                 iteration + 1) + ".csv"  # this must be done 5 times (or the number of clusters) for each iteration
-            np.savetxt(filename, sigma[j], delimiter=",")
+            np.savetxt(filename, calc_sigma[j], delimiter=",")
 
 
 def k_means(data, k_clusters):
